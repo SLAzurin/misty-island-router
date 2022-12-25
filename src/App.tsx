ba160d3bd67251314edaa2a8dde74ddcc119e5cf
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2022-12-25 00:37:01 +0000

    Add inital UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { items, sampleBuild } from "./data";
 
 function App() {
+  const [build, setBuild] = useState<string[][]>(
+    localStorage.getItem("mistyislandbuild") !== null
+      ? JSON.parse(localStorage.getItem("mistyislandbuild") as string)
+      : sampleBuild
+  );
+
+  const getRawMaterials = (
+    structures: Array<string>
+  ): { [key: string]: number } => {
+    let rawMaterials: { [key: string]: number } = {};
+    console.log(structures)
+    structures.forEach((structure) => {
+      console.log("onto", structure)
+      for (const [rawMaterial, count] of Object.entries(items[structure])) {
+        if (typeof rawMaterials[rawMaterial] === "undefined")
+          rawMaterials[rawMaterial] = 0;
+        rawMaterials[rawMaterial] += count;
+      }
+    });
+    return rawMaterials;
+  };
+
+  useEffect(() => {
+    localStorage.setItem("mistyislandbuild", JSON.stringify(build));
+  }, [build]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        {build.map((back, backNumber) => {
+          return (
+            <div
+              key={backNumber}
+              style={{ display: "flex", flexDirection: "row" }}
+            >
+              <div>
+                <h2>Back #{backNumber + 1}</h2>
+                {back.map((structure, structureIndex) => {
+                  return (
+                    <div key={structureIndex}>
+                      <button
+                        type="button"
+                        onClick={() => {
+                          console.log("Clicked Delete", structureIndex);
+                        }}
+                        style={{ marginRight: "10px" }}
+                      >
+                        Delete
+                      </button>
+                      <select defaultValue={structure}>
+                        {Object.keys(items)
+                          .sort()
+                          .map((itemName, itemIndex) => {
+                            // fake indices
+                            return (
+                              <option
+                                key={`${backNumber}_${structureIndex}_${itemIndex}`}
+                                value={itemName}
+                              >
+                                {itemName}
+                              </option>
+                            );
+                          })}
+                      </select>
+                    </div>
+                  );
+                })}
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log("Click Add structure", backNumber + 1);
+                  }}
+                >
+                  Add craftable item
+                </button>
+              </div>
+              <div style={{ marginLeft: "40px" }}>
+                <h2>Back #{backNumber + 1} raw material cost:</h2>
+                {Object.entries(getRawMaterials(back)).map(
+                  ([rawMaterial, count]) => {
+                    return (
+                      <div>
+                        {count} {rawMaterial}
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        onClick={() => {
+          console.log("Add back");
+        }}
+      >
+        Add back
+      </button>
     </div>
   );
 }
---
+++ src/data/index.ts
@@ -0,0 +1,28 @@
+export { items } from './items'
+export const sampleBuild = [
+    [
+        "Survival Worktable",
+        "Rainwater Filter",
+        "Industrial Work Table",
+        "Countertop",
+        "Gold hammer",
+        "Gold pick",
+        "Gold axe",
+        "Rope sandals",
+        "Bucket",
+        "Leaf Bag",
+        "Leaf Bag"
+    ],
+    [
+        "Engineering work table",
+        "Alchemy pot",
+        "Hand Cannon",
+        "Heavy Armor Battle Suit",
+        "Steel Helmet",
+        "Leather bag",
+        "Health Potion 2",
+    ],
+    [
+        // add concrete wall x4
+    ]
+]
