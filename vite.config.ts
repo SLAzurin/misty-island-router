c3f443fbdf7e0052c6f6fe3685b7b8b75c7417c2
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-07-23 21:46:43 -0400

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/misty-island-router/'
+  // base: '/misty-island-router-test/'
 })
