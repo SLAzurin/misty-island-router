0cce1fa08bb566eed953a8248b04c09ae657ddd9
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-01-18 00:21:45 -0500

    Add query for stats

--- statistics_server/sqlfiles/db.sql
+++ statistics_server/sqlfiles/db.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS visits (
   dt timestamp with time zone not null default now()
 );
 create index if not exists visits_index on visits(id);
+
+-- query for stats
+-- SELECT id, COUNT(id) as connects, TIMEZONE('America/New_York', MAX(dt)) AS last_visit FROM visits group by id having count(id) > 2 order by connects desc, last_visit  desc;
